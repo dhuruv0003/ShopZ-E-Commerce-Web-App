f0f1992aa8db6f8c1b295c22fc8573469299e561
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-06 18:39:45 +0530

    new commit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,10 +13,10 @@ const CartItem = ({item, itemIndex}) => {
   }
 
   return (
-    <div className="flex items-center p-2 md:p-5 justify-between   mt-2 mb-2 md:mx-5 ">
-      <div className="flex flex-col md:flex-row p-0 md:p-3 gap-5 items-center">
-        <div className="w-[30%]">
-          <img className="object-cover " src={item.image} />
+    <div className=" ">
+      <div className="">
+        <div className="">
+          <img className="" src={item.image} />
         </div>
         <div className="md:ml-10 self-start space-y-5 w-[100%] md:w-[70%]">
           <h1 className="text-xl text-slate-700 font-semibold">{item.title}</h1>
