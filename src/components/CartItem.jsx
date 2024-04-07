db560141199f2db9cd1842ec2cbf6808addc2859
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-07 15:17:43 +0530

    new commit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,26 +13,23 @@ const CartItem = ({item, itemIndex}) => {
   }
 
   return (
-    <div className="w-3/4 flex flex-col justify-between items-center  ">
-      <div className="">
-        <div className="h-[180px]">
-          <img className="h-full" src={item.image} />
+    <div className=" flex items-center p-2 md:p-5 justify-between shadow-[0_3px_10px_rgb(0,0,0,0.2)]  mt-2 mb-2 md:mx-5 ">
+     
+      <div className="flex flex-col md:flex-row p-0 md:p-3 gap-5 items-center">
+        <div className="w-[30%]">
+          <img className="object-cover hover:scale-110 transition-all duration-300" src={item.image} />
         </div>
-        <div className="text-justify">
-          <h1 className="">{item.title.split(" ").slice(0,6).join(" ")+".."}</h1>
-          <h1 className="">{item.description.split(" ").slice(0,7).join(" ")+"..."}</h1>
-          <div className="">
-            <p className="">{item.price}</p>
-            <div className=""
+        <div className="md:ml-10 self-start space-y-5 w-[100%] md:w-[70%]">
+          <h1 className="text-xl text-slate-700 font-semibold">{item.title.split(" ").slice(0,6).join(" ")+".."}</h1>
+          <h1 className="text-base text-slate-700 font-medium">{item.description.split(" ").slice(0,7).join(" ")+"..."}</h1>
+          <div className="flex items-center justify-between">
+            <p className="font-bold text-lg text-green-600 ">${item.price}</p>
+            <div className="text-red-800  bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
             onClick={removeFromCart}>
               <AiFillDelete/>
             </div>
           </div>
-          <div className="w-full h-1 my-4 bg-black"></div>
-
-        </div>
-
-
+        </div> 
       </div>
 
     </div>
