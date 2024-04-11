36499eaa9fe04e8bfe1a471291b755690c341865
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-12 05:09:55 +0530

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header({ data }) {
     return (
         <div className="w-[100%] overflow-x-hidden sm:h-[100vh] flex justify-center  rounded-b-[20%] border-b-8 border-black bg-slate-600 my-[5rem] space-x-6 px-8">
             <div className="rounded-full flex flex-col gap-6  max-w-[1200px] mx-auto justify-evenly items-center">
-                <div className=" h-fit flex-col shadow-white shadow-lg bg-white  text-slate-800 w-full p-9 flex gap-4">
+                <div className=" h-fit flex-col shadow-white shadow-lg bg-white  text-slate-800 w-full p-9 flex gap-4 my-5">
                     <h1 className=" text-center text-3xl " >Hey GenZ Welcome To </h1>
                     <h1 className="text-8xl  text-center">ShopZ</h1></div>
                 <div className="text-center flex flex-wrap  text-black gap-3">
