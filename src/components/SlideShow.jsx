3c4f19c49b91d915fe0f129dac6e8e4a283e2d4d
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-12 16:25:16 +0530

    new commit

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -6,6 +6,7 @@ export default function SlideShow({ price,  info, image, name}) {
             <div className=" w-full flex justify-center items-center" ><img className=" rounded-t-full h-[120px]  object-cover " src={image} alt="" /></div>
             <div className="text-green-600 font-extrabold">${price}</div>
             <div className="">{info}</div>
+            
         </div>
     )
 }
--- src/pages/data.jsx
+++ src/pages/data.jsx
@@ -4,14 +4,14 @@ const data = [
 		name:"Fjallraven - Folds",
 		info: "Your perfect pack for everyday use and walks in the... ",
 		image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-		price: "35,758",
+		price: "758",
 	},
 	{
 		id: 2,
 		name: "Mens Casual ",
 		info: `Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light... `,
 		image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-		price: "82,560",
+		price: "560",
 	},
 	{
 		id: 3,
@@ -25,7 +25,7 @@ const data = [
 		name: "Solid Gold Petite",
 		info: "Satisfaction Guaranteed. Return or exchange any order within 30 days. ",
 		image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-		price: "31,095",
+		price: "1,095",
 	},
 	
 ];
