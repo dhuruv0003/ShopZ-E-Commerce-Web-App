05e1ed38cd2e067579e0c398d973f3c7605745d5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-12 16:04:41 +0530

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header({ data }) {
             <div className="rounded-full flex flex-col gap-6  max-w-[1200px] mx-auto justify-evenly items-center" >
                 <div className=" h-fit flex-col shadow-black shadow-lg bg-white   w-full p-14 flex gap-4 my-5 bg-cover bg-no-repeat transition-all duration-300 ease-in-out " style={{backgroundImage: `url(${"https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp"})`}}>
                     <h1 className=" text-center text-3xl font-bold bg-gradient-to-r from-red-200 to-purple-100 text-transparent bg-clip-text" >Hey GenZ Welcome To SHOPZ</h1>
-                    <h1 className="text-5xl  text-center text-white font-bold">The Only Solution Your needs</h1></div>
+                    <h1 className="text-5xl  text-center text-white font-bold">The Only Solution for Your needs</h1></div>
 
                 <div className="text-center flex flex-wrap  text-black gap-3">
 
