a8236109bfaa2c333b717deebe6471c496d7a211
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-05-12 19:11:18 +0530

    new commit

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -52,57 +52,59 @@ export default function ProductItem() {
     };
 
     return (
-        <div className='py-[10rem] px-[2rem] max-w-[1000px] mx-auto flex flex-wrap justify-center gap-[5rem] bg-gradient-to-r  from-blue-100 to-purple-200  h-fit '>
+        <div className="bg-gradient-to-r  from-blue-100 to-purple-200 ">
+            <div className='py-[10rem] max-w-[1000px]  mx-auto flex flex-wrap justify-center gap-[5rem] bg-gradient-to-r  from-blue-100 to-purple-200 h-fit '>
 
-            <div className="sm:w-[30%]">
-                <img src={product.image} className="h-[400px] w-[350px]" />
-            </div>
-
-            <div className="flex flex-col justify-evenly gap-[2rem] sm:w-[60%] h-fit capitalize ">
-                <div>
-                    <p className="text-gray-700 font-bold text-3xl text-left truncate  mt-1">{product.category}</p>
-                </div>
-                <div>
-                    <p className="text-gray-700 font-bold text-xl text-left  mt-1">{product.title}</p>
-                </div>
-                <div className='w-[300px] sm:w-full'>
-                    <p className="text-gray-00 font-normal text-[15px] text-left  ">{product.description}</p>
+                <div className="sm:w-[30%]">
+                    <img src={product.image} className="h-[400px] w-[350px]" />
                 </div>
 
-                <p className=" text-green-600 font-bold text-2xl">${product.price}</p>
+                <div className="flex flex-col justify-evenly gap-[2rem] sm:w-[60%] h-fit capitalize ">
+                    <div>
+                        <p className="text-gray-700 font-bold text-3xl text-left truncate  mt-1">{product.category}</p>
+                    </div>
+                    <div>
+                        <p className="text-gray-700 font-bold text-xl text-left  mt-1">{product.title}</p>
+                    </div>
+                    <div className='w-[300px] sm:w-full'>
+                        <p className="text-gray-00 font-normal text-[15px] text-left  ">{product.description}</p>
+                    </div>
 
-                <div>
-                    <p className="text-gray-700 font-bold text-xl flex gap-1 items-center text-left  mt-1">ratings : {product.rating.rate}<FaStar /> ({product.rating.count})</p>
+                    <p className=" text-green-600 font-bold text-2xl">${product.price}</p>
+
+                    <div>
+                        <p className="text-gray-700 font-bold text-xl flex gap-1 items-center text-left  mt-1">ratings : {product.rating.rate}<FaStar /> ({product.rating.count})</p>
+                    </div>
+
+
+                    <button className="text-gray-700 border-2 border-gray-700 text-lg font-bold rounded-full px-2 w-[200px] p-1" onClick={() => setProduct(product)} >Buy Now</button>
+
+
+                </div>
+
+                <div className="w-3/4 h-[400px]">
+                    <Slider {...settings}>
+                        {
+                            data.map((slide, index) => {
+                                return (
+                                    <div key={index} className="w-[90%] mx-auto  lg:w-[24%] ">
+                                        <SlideShow key={slide.id}
+                                            {...slide} />
+
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </Slider>;
                 </div>
 
 
-                <button className="text-gray-700 border-2 border-gray-700 text-lg font-bold rounded-full px-2 w-[200px] p-1" onClick={() => setProduct(product)} >Buy Now</button>
-
-
-            </div>
-
-            <div className="w-3/4 h-[400px]">
-                <Slider {...settings}>
-                    {
-                        data.map((slide, index) => {
-                            return (
-                                <div key={index} className="w-[90%] mx-auto  lg:w-[24%] ">
-                                    <SlideShow key={slide.id}
-                                        {...slide} />
-
-                                </div>
-
-                            )
-                        })
-                    }
-                </Slider>;
-            </div>
 
 
 
-
-
-        </div >
+            </div >
+        </div>
 
     )
 }
