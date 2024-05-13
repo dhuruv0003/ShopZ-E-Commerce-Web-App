0919b6adc43e446bf9e96528db51dc57a220cee0
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-05-14 01:56:44 +0530

    new commit

--- src/pages/data.jsx
+++ src/pages/data.jsx
@@ -119,30 +119,7 @@ const data = [
 		  "count": 470
 		}
 	  },
-	  {
-		"id": 11,
-		"title": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
-		"price": 109,
-		"description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
-		"category": "electronics",
-		"image": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
-		"rating": {
-		  "rate": 4.8,
-		  "count": 319
-		}
-	  },
-	  {
-		"id": 12,
-		"title": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
-		"price": 114,
-		"description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-		"category": "electronics",
-		"image": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
-		"rating": {
-		  "rate": 4.8,
-		  "count": 400
-		}
-	  }
+	 
 ];
 
 export default data;
