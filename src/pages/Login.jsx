e7823cfa3edd352b5c438fb8265b86b272c5b059
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-02 15:44:17 +0530

    new commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
         image={loginimg}
       />
       <footer className={`${isDarkMode ? 'bg-gradient-to-r from-slate-400 to-slate-950 shadow-slate-900 shadow-lg text-white z-50 w-full ' : 'bg-gradient-to-r from-pink-100 to-indigo-500 shadow-black shadow-lg  z-50 w-full '}max-[600px]:py-6 py-6 mt-10 max-[600px]:flex sm:flex sm:flex-col gap-5 `}>
-        <div className="max-[600px]:flex max-[600px]:flex-col  flex justify-center  max-[600px]:gap-1 gap-5 items-center max-[600px]:text-lg sm:text-xl px-10 font-extrabold ">
+        <div className="max-[600px]:flex max-[600px]:flex-col  flex justify-center  max-[600px]:gap-1 gap-5 items-center max-[600px]:text-lg sm:text-xl px-8 font-extrabold ">
           <NavLink to='/'>
             Home
           </NavLink>
